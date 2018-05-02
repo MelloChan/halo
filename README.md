87a30083a5b76bdb9fd3462cb26fd45a0e5ce674
MelloYuki <352983198@qq.com>
2018-05-02 16:44:25 +0800

    :tada: 初始化~

---
+++ README.md
@@ -0,0 +1,15 @@
+## halo手机商城服务端
+
+### 技术选型  
+
+框架: Spring Boot、Mybatis  
+数据库: MySQL5.7  
+缓存服务器: Redis3.2  
+应用服务器: Tomcat8  
+web服务器: Nginx1.12.0  
+开发环境: Windows7/8/10  
+部署环境: CentOS6.6  
+构建工具: Maven3  
+开发工具: IDEA2017/2018  
+版本控制: Git   
+编程规约: 参考阿里巴巴Java开发手册
---
+++ src/main/java/com/halo/HaloServerApplication.java
@@ -0,0 +1,12 @@
+package com.halo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaloServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaloServerApplication.class, args);
+	}
+}
