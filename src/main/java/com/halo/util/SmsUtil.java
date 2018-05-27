f6fd3ad68e787cc6951c3479e1160cded4e1f363
MelloYuki <352983198@qq.com>
2018-05-27 16:56:25 +0800

    :art: format of the code.

--- src/main/java/com/halo/util/SmsUtil.java
+++ src/main/java/com/halo/util/SmsUtil.java
@@ -8,21 +8,18 @@ import com.halo.config.properties.Ucpaas;
  * 短信发送工具
  */
 public class SmsUtil {
-    private SmsUtil(){}
+    private SmsUtil() {
+    }
 
     /**
      * 短信单发验证
+     *
      * @param ucpaas 云之讯配置类
      * @return 结果json
      */
-    public static String sendSms(Ucpaas ucpaas){
-        String sid=ucpaas.getSid();
-        String token=ucpaas.getToken();
-        String appid=ucpaas.getAppid();
-        String templateid=ucpaas.getTemplateid();
-        String param=ucpaas.getParam();
-        String mobile=ucpaas.getMobile();
-        String url=ucpaas.getUrl();
-        return HttpUtil.getInstance().requestPostByJson(url,GsonUtil.toJsonString(ucpaas));
+    public static String sendSms(Ucpaas ucpaas) {
+        String url = ucpaas.getUrl();
+        String json = GsonUtil.toJsonString(ucpaas);
+        return HttpUtil.getInstance().requestPostByJson(url, json);
     }
 }
